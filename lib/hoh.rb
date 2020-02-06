2d5df934bfb3682e56f53f6ebb615cce68beabad
jolly-assembler-1925 <jnzhang622@gmail.com>
2020-02-06 00:21:22 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -41,5 +41,6 @@ def naming_system
   #  Species
   # So, if we have the "Kingdom" node we should be able to "tunnel" into the
   # HoH all the way to Species!
-return [:Kingdom][:Phylum][:Class][:Order][:Family][:Genus][:Species]
+[:Kingdom][:Phylum][:Class][:Order][:Family][:Genus][:Species]
+return 
 end
